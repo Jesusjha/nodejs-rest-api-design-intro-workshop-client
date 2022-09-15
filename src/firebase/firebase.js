01ee0f3f33b49a5ff71e1d3ed5926af9e37cb644
Jesus Herrera <heisusin@gmail.com>
2022-09-15 19:13:46 +0200

    perf: ⚡️ Created environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,59 +1,63 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// const dotenv = require("dotenv");
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 if (!firebase.apps.length) {
-  // Paste your config object here ⬇️
-  const firebaseConfig = {
-    apiKey: "AIzaSyBpjUjFAt7RZAVFda1OBJCHRYssLzdlJYY",
-    authDomain: "nodejs-firebase-app-408c6.firebaseapp.com",
-    projectId: "nodejs-firebase-app-408c6",
-    storageBucket: "nodejs-firebase-app-408c6.appspot.com",
-    messagingSenderId: "370587034017",
-    appId: "1:370587034017:web:aabc0671314c2d40d0abe0",
+	// Paste your config object here ⬇️
+	const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKECT,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
-
-  firebase.initializeApp(firebaseConfig);
+  console.log(firebaseConfig)
+	firebase.initializeApp(firebaseConfig);
 } else {
-  // if already initialized, use that one
-  firebase.app();
+	// if already initialized, use that one
+	firebase.app();
 }
 
 export const auth = firebase.auth();
 
 export function singInWithGoogle() {
-  const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
+	const GoogleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-  return auth.signInWithPopup(GoogleAuthProvider);
+	return auth.signInWithPopup(GoogleAuthProvider);
 }
 
 export function singInWithEmailAndPassword(email, password) {
-  return auth.signInWithEmailAndPassword(email, password);
+	return auth.signInWithEmailAndPassword(email, password);
 }
 
 export function singUpWithEmailAndPassword(email, password) {
-  return auth.createUserWithEmailAndPassword(email, password);
+	return auth.createUserWithEmailAndPassword(email, password);
 }
 
 export function sendPasswordResetEmail(email) {
-  return auth.sendPasswordResetEmail(email);
+	return auth.sendPasswordResetEmail(email);
 }
 
 export function signOut() {
-  return auth.signOut();
+	return auth.signOut();
 }
 
 export function getCurrentUserToken() {
-  if (!auth.currentUser) {
-    return null;
-  }
+	if (!auth.currentUser) {
+		return null;
+	}
 
-  return auth.currentUser.getIdToken();
+	return auth.currentUser.getIdToken();
 }
 
 export function getCurrentUserEmail() {
-  if (!auth.currentUser) {
-    return null;
-  }
+	if (!auth.currentUser) {
+		return null;
+	}
 
-  return auth.currentUser.email;
+	return auth.currentUser.email;
 }
